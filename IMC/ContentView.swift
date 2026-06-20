0c398066dd617c0c8841b526e1574def9a4586a2
Willane Paiva <willanepaiva@hotmail.com>
2026-06-20 08:30:34 -0300

    Initial Commit

---
+++ IMC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IMC
+//
+//  Created by Willane Paiva on 20/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IMC/IMCApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMCApp.swift
+//  IMC
+//
+//  Created by Willane Paiva on 20/06/26.
+//
+
+import SwiftUI
+
+@main
+struct IMCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
